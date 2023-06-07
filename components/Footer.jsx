9619e99081c0c5bd35bd7079e7ddc93c8ca838a6
Nasr Souid <91200028+souid-nasr@users.noreply.github.com>
2023-06-07 23:50:18 +0100

    footer links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,72 +1,148 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 function Footer() {
   return (
     <div>
-          <footer id="footer" className="footer">
+      <footer id="footer" className="footer">
+        <div className="container">
+          <div className="row gy-4">
+            <div className="col-lg-5 col-md-12 footer-info">
+              <Link
+                href="/"
+                className="logo nav-link d-flex align-items-center"
+              >
+                <span>Unik Espace</span>
+              </Link>
+              <p>AGENCEMENT & RANGEMENT INTÉRIEUR</p>
+              <p>Sublimez votre intérieur afin qu&#39;il vous ressemble ! ! </p>
+              <p>Qualité & Sur-mesure </p>
+              <div className="social-links d-flex mt-4">
+                <a
+                  href="https://www.facebook.com/Unikespace/"
+                  className="facebook"
+                >
+                  <i className="bi bi-facebook"></i>
+                </a>
+                <a href="#" className="instagram">
+                  <i className="bi bi-instagram"></i>
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-2 col-6 footer-links">
+              <h4>Liens utiles</h4>
+              <ul>
+                <li>
+                  <Link className="nav-link" href="/">
+                    Accueil
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" href="/about">
+                    Notre Societe
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" href="/agencements">
+                    Amenagements
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" href="/portfolio">
+                    Realisations
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" href="/pricing">
+                    Pricing
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" href="/contact">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
 
-<div className="container">
-  <div className="row gy-4">
-    <div className="col-lg-5 col-md-12 footer-info">
-      <a href="index.html" className="logo d-flex align-items-center">
-        <span>Unik Espace</span>
-      </a>
-      <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
-      <div className="social-links d-flex mt-4">
-        <a href="#" className="twitter"><i className="bi bi-twitter"></i></a>
-        <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
-        <a href="#" className="instagram"><i className="bi bi-instagram"></i></a>
-        <a href="#" className="linkedin"><i className="bi bi-linkedin"></i></a>
-      </div>
+            <div className="col-lg-2 col-6 footer-links">
+              <h4>Nos Agencements</h4>
+              <ul>
+                <li>
+                  <Link className="nav-link" href="/portfolio/cuisines">
+                    Cuisines
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" href="/portfolio/dressings">
+                    Dressings
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" href="/portfolio/bibliotheques">
+                    Bibliotheques
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" href="/portfolio/habillages">
+                    Habillages Mural
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" href="/portfolio/sousescslier">
+                    Sous Escaliers
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" href="/portfolio/separations">
+                    Separations
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" href="/portfolio/chambres">
+                    Chambres d&#39;enfants
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" href="/portfolio/portes">
+                    Portes
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+              <h4>Contact Us</h4>
+              <p>
+                P8P4+JX
+                <br />
+                Ez Zahra
+                <br />
+                Ben Arous <br />
+                <br />
+                <strong>Tel:</strong>+216 98 188 622
+                <br />
+                <strong>Email:</strong>contact@unik-espace.com
+                <br />
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="container mt-4">
+          <div className="copyright">
+            &copy; Copyright{" "}
+            <strong>
+              <span><a href="https://unik-group.com/">Unik Group</a></span>
+            </strong>
+            . All Rights Reserved
+          </div>
+          <div className="credits">
+            Designed by <a href="https://nasrsouid.vercel.app/">Nasr</a>
+          </div>
+        </div>
+      </footer>
     </div>
-    <div className="col-lg-2 col-6 footer-links">
-      <h4>Useful Links</h4>
-      <ul>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/about">About us</Link></li>
-        <li><Link href="/services">Services</Link></li>
-        <li><Link href="/pricing">Pricing</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
-      </ul>
-    </div>
-
-    <div className="col-lg-2 col-6 footer-links">
-      <h4>Our Services</h4>
-      <ul>
-        <li><Link href="#">Web Design</Link></li>
-        <li><Link href="#">Web Development</Link></li>
-        <li><Link href="#">Product Management</Link></li>
-        <li><Link href="#">Marketing</Link></li>
-        <li><Link href="#">Graphic Design</Link></li>
-      </ul>
-    </div>
-
-    <div className="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-      <h4>Contact Us</h4>
-      <p>
-        A108 Adam Street <br/>
-        New York, NY 535022<br/>
-        United States <br/><br/>
-        <strong>Phone:</strong> +1 5589 55488 55<br/>
-        <strong>Email:</strong> info@example.com<br/>
-      </p>
-
-    </div>
-  </div>
-</div>
-
-<div className="container mt-4">
-  <div className="copyright">
-    &copy; Copyright <strong><span>Unik Group</span></strong>. All Rights Reserved
-  </div>
-  <div className="credits">
-  Designed by <a href="https://nasrsouid.vercel.app/">Nasr</a>
-  </div>
-</div>
-
-</footer>
-    </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- components/Hero2.jsx
+++ components/Hero2.jsx
@@ -29,199 +29,190 @@ const Slider = ({ slides }) => {
       spaceBetween={0}
       slidesPerView={1}
     >
-
-        <SwiperSlide >
-          <section
-            id="hero"
-            className="hero bg-[url('../assets/img/cuisine/kitchen_1920.jpg')] bg-cover	"
-          >
-            <div className="text-white uppercase bg-black/50 ">
-              <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
-                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
-                  Cuisines{" "}
-                </h1>
-                <div className="flex justify-center items-center"></div>
-                <p className="md:text-2xl text-xl font-bold text-white">
-                  Monitor your data analytics to increase revenue for BTB, BTC,
-                  & SASS platforms.
-                </p>
-                <Link href="/portfolio/cuisines">
-                  <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
-                    voir toutes les cuisines{" "}
-                  </button>
-                </Link>
-              </div>
+      <SwiperSlide>
+        <section
+          id="hero"
+          className="hero bg-[url('../assets/img/cuisine/kitchen_1920.jpg')] bg-cover	"
+        >
+          <div className="text-white uppercase bg-black/50 ">
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
+              <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
+                Cuisines{" "}
+              </h1>
+              <div className="flex justify-center items-center"></div>
+              <p className="md:text-2xl text-xl font-bold text-white">
+                Sublimez votre intérieur afin qu&#39;il vous ressemble ! !
+              </p>
+              <Link href="/portfolio/cuisines">
+                <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
+                  voir toutes les cuisines{" "}
+                </button>
+              </Link>
             </div>
-          </section>{" "}
-        </SwiperSlide>
-        <SwiperSlide >
-          <section
-            id="hero"
-            className="hero bg-[url('../assets/img/dressing/dressing_1280.jpg')] bg-cover	"
-          >
-            <div className="text-white uppercase bg-black/50 ">
-              <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
-                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
-                  Dressings{" "}
-                </h1>
-                <div className="flex justify-center items-center"></div>
-                <p className="md:text-2xl text-xl font-bold text-white">
-                  Monitor your data analytics to increase revenue for BTB, BTC,
-                  & SASS platforms.
-                </p>
-                <Link href="/portfolio/dressings">
-                  <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
-                    voir toutes les dressings{" "}
-                  </button>
-                </Link>
-              </div>
+          </div>
+        </section>{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        <section
+          id="hero"
+          className="hero bg-[url('../assets/img/dressing/dressing_1280.jpg')] bg-cover	"
+        >
+          <div className="text-white uppercase bg-black/50 ">
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
+              <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
+                Dressings{" "}
+              </h1>
+              <div className="flex justify-center items-center"></div>
+              <p className="md:text-2xl text-xl font-bold text-white">
+                Sublimez votre intérieur afin qu&#39;il vous ressemble ! !
+              </p>
+              <Link href="/portfolio/dressings">
+                <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
+                  voir toutes les dressings{" "}
+                </button>
+              </Link>
             </div>
-          </section>{" "}
-        </SwiperSlide>
-        <SwiperSlide >
-          <section
-            id="hero"
-            className="hero bg-[url('../assets/img/bibliotheque/bibliotheque_1.jpg')] bg-cover	"
-          >
-            <div className="text-white uppercase bg-black/50 ">
-              <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
-                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
-                  Bibliothèques{" "}
-                </h1>
-                <div className="flex justify-center items-center"></div>
-                <p className="md:text-2xl text-xl font-bold text-white">
-                  Monitor your data analytics to increase revenue for BTB, BTC,
-                  & SASS platforms.
-                </p>
-                <Link href="/portfolio/bibliotheques">
-                  <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
-                    voir toutes les bibliothèques{" "}
-                  </button>
-                </Link>
-              </div>
+          </div>
+        </section>{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        <section
+          id="hero"
+          className="hero bg-[url('../assets/img/bibliotheque/bibliotheque_1.jpg')] bg-cover	"
+        >
+          <div className="text-white uppercase bg-black/50 ">
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
+              <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
+                Bibliothèques{" "}
+              </h1>
+              <div className="flex justify-center items-center"></div>
+              <p className="md:text-2xl text-xl font-bold text-white">
+                Sublimez votre intérieur afin qu&#39;il vous ressemble ! !
+              </p>
+              <Link href="/portfolio/bibliotheques">
+                <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
+                  voir toutes les bibliothèques{" "}
+                </button>
+              </Link>
             </div>
-          </section>{" "}
-        </SwiperSlide>
-        <SwiperSlide >
-          <section
-            id="hero"
-            className="hero bg-[url('../assets/img/chambre/chambre.jpg')] bg-cover	"
-          >
-            <div className="text-white uppercase bg-black/50 ">
-              <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
-                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
-                  Chambres d&#39;enfants {" "}
-                </h1>
-                <div className="flex justify-center items-center"></div>
-                <p className="md:text-2xl text-xl font-bold text-white">
-                  Monitor your data analytics to increase revenue for BTB, BTC,
-                  & SASS platforms.
-                </p>
-                <Link href="/portfolio/chambres">
-                  <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
-                    voir toutes les Chambres d&#39;enfants{" "}
-                  </button>
-                </Link>
-              </div>
+          </div>
+        </section>{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        <section
+          id="hero"
+          className="hero bg-[url('../assets/img/chambre/chambre.jpg')] bg-cover	"
+        >
+          <div className="text-white uppercase bg-black/50 ">
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
+              <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
+                Chambres d&#39;enfants{" "}
+              </h1>
+              <div className="flex justify-center items-center"></div>
+              <p className="md:text-2xl text-xl font-bold text-white">
+                Sublimez votre intérieur afin qu&#39;il vous ressemble ! !
+              </p>
+              <Link href="/portfolio/chambres">
+                <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
+                  voir toutes les Chambres d&#39;enfants{" "}
+                </button>
+              </Link>
             </div>
-          </section>{" "}
-        </SwiperSlide>
-        <SwiperSlide >
-          <section
-            id="hero"
-            className="hero bg-[url('../assets/img/habillage/habillage.jpg')] bg-cover	"
-          >
-            <div className="text-white uppercase bg-black/50 ">
-              <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
-                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
-                  Habillages {" "}
-                </h1>
-                <div className="flex justify-center items-center"></div>
-                <p className="md:text-2xl text-xl font-bold text-white">
-                  Monitor your data analytics to increase revenue for BTB, BTC,
-                  & SASS platforms.
-                </p>
-                <Link href="/portfolio/habillages">
-                  <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
-                    voir toutes les Habillages{" "}
-                  </button>
-                </Link>
-              </div>
+          </div>
+        </section>{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        <section
+          id="hero"
+          className="hero bg-[url('../assets/img/habillage/habillage.jpg')] bg-cover	"
+        >
+          <div className="text-white uppercase bg-black/50 ">
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
+              <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
+                Habillages{" "}
+              </h1>
+              <div className="flex justify-center items-center"></div>
+              <p className="md:text-2xl text-xl font-bold text-white">
+                Sublimez votre intérieur afin qu&#39;il vous ressemble ! !
+              </p>
+              <Link href="/portfolio/habillages">
+                <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
+                  voir toutes les Habillages{" "}
+                </button>
+              </Link>
             </div>
-          </section>{" "}
-        </SwiperSlide>
-        <SwiperSlide >
-          <section
-            id="hero"
-            className="hero bg-[url('../assets/img/porte/porte.jpg')] bg-cover	"
-          >
-            <div className="text-white uppercase bg-black/50 ">
-              <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
-                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
-                  Portes {" "}
-                </h1>
-                <div className="flex justify-center items-center"></div>
-                <p className="md:text-2xl text-xl font-bold text-white">
-                  Monitor your data analytics to increase revenue for BTB, BTC,
-                  & SASS platforms.
-                </p>
-                <Link href="/portfolio/portes">
-                  <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
-                    voir toutes les Portes{" "}
-                  </button>
-                </Link>
-              </div>
+          </div>
+        </section>{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        <section
+          id="hero"
+          className="hero bg-[url('../assets/img/porte/porte.jpg')] bg-cover	"
+        >
+          <div className="text-white uppercase bg-black/50 ">
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
+              <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
+                Portes{" "}
+              </h1>
+              <div className="flex justify-center items-center"></div>
+              <p className="md:text-2xl text-xl font-bold text-white">
+                Sublimez votre intérieur afin qu&#39;il vous ressemble ! !
+              </p>
+              <Link href="/portfolio/portes">
+                <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
+                  voir toutes les Portes{" "}
+                </button>
+              </Link>
             </div>
-          </section>{" "}
-        </SwiperSlide>
-        <SwiperSlide >
-          <section
-            id="hero"
-            className="hero bg-[url('../assets/img/separation/separation.jpg')] bg-cover	"
-          >
-            <div className="text-white uppercase bg-black/50 ">
-              <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
-                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
-                  Separations {" "}
-                </h1>
-                <div className="flex justify-center items-center"></div>
-                <p className="md:text-2xl text-xl font-bold text-white">
-                  Monitor your data analytics to increase revenue for BTB, BTC,
-                  & SASS platforms.
-                </p>
-                <Link href="/portfolio/separations">
-                  <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
-                    voir toutes les Separations{" "}
-                  </button>
-                </Link>
-              </div>
+          </div>
+        </section>{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        <section
+          id="hero"
+          className="hero bg-[url('../assets/img/separation/separation.jpg')] bg-cover	"
+        >
+          <div className="text-white uppercase bg-black/50 ">
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
+              <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
+                Separations{" "}
+              </h1>
+              <div className="flex justify-center items-center"></div>
+              <p className="md:text-2xl text-xl font-bold text-white">
+                Sublimez votre intérieur afin qu&#39;il vous ressemble ! !
+              </p>
+              <Link href="/portfolio/separations">
+                <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
+                  voir toutes les Separations{" "}
+                </button>
+              </Link>
             </div>
-          </section>{" "}
-        </SwiperSlide>
-        <SwiperSlide >
-          <section
-            id="hero"
-            className="hero bg-[url('../assets/img/sousescalier/sousescalier.jpg')] bg-cover	"
-          >
-            <div className="text-white uppercase bg-black/50 ">
-              <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
-                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
-                Sous escaliers {" "}
-                </h1>
-                <div className="flex justify-center items-center"></div>
-                <p className="md:text-2xl text-xl font-bold text-white">
-                  Monitor your data analytics to increase revenue for BTB, BTC,
-                  & SASS platforms.
-                </p>
-                <Link href="/portfolio/sousescaliers">
-                  <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
-                    voir toutes les Sous escaliers{" "}
-                  </button>
-                </Link>
-              </div>
+          </div>
+        </section>{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        <section
+          id="hero"
+          className="hero bg-[url('../assets/img/sousescalier/sousescalier.jpg')] bg-cover	"
+        >
+          <div className="text-white uppercase bg-black/50 ">
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
+              <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-2">
+                Sous escaliers{" "}
+              </h1>
+              <div className="flex justify-center items-center"></div>
+              <p className="md:text-2xl text-xl font-bold text-white">
+                Sublimez votre intérieur afin qu&#39;il vous ressemble ! !
+              </p>
+              <Link href="/portfolio/sousescaliers">
+                <button className="uppercase  rounded-full font-medium my-6 px-3 py-3 border-red border-2 text-white">
+                  voir toutes les Sous escaliers{" "}
+                </button>
+              </Link>
             </div>
-          </section>{" "}
-        </SwiperSlide>
+          </div>
+        </section>{" "}
+      </SwiperSlide>
     </Swiper>
   );
 };
